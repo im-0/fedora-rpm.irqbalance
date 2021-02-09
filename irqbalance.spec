liutgnu/fc-irqbalance

commit 76add4e1b859699ad90e6438dd6362a284191b93

Resolves: bz1866002

irqbalance.spec
@@ -1,7 +1,7 @@
 Name:           irqbalance
-Version:        1.6.0
+Version:        1.7.0
 Release:        3%{?dist}
-Epoch:          2
+Epoch:          1 
 Summary:        IRQ balancing daemon
 
 License:        GPLv2
@@ -22,7 +22,6 @@ Requires: numactl-libs
 
 ExcludeArch: s390 s390x
 
-Patch1: irqbalance-1.6.0-env-file-path.patch
 
 %description
 irqbalance is a daemon that evenly distributes IRQ load across
@@ -30,7 +29,6 @@ multiple CPUs for enhanced performance.
 
 %prep
 %setup -q
-%patch1 -p1
 
 %build
 ./autogen.sh
@@ -71,6 +69,9 @@ fi
 /sbin/chkconfig --del irqbalance >/dev/null 2>&1 || :
 
 %changelog
+* Tue Aug 04 2020 Neil Horman <nhorman@redhat.com> - 2:1.7.0-1
+- Update to latest upstream (bz 1866002)
+
 * Tue Jul 28 2020 Fedora Release Engineering <releng@fedoraproject.org> - 2:1.6.0-3
 - Rebuilt for https://fedoraproject.org/wiki/Fedora_33_Mass_Rebuild
 

.gitignore
@@ -16,3 +16,4 @@ irqbalance-0.56.tbz2
 /irqbalance-1.3.0.tar.gz
 /irqbalance-1.4.0.tar.gz
 /irqbalance-1.6.0.tar.gz
+/irqbalance-1.7.0.tar.gz

sources
@@ -1,1 +1,1 @@
-SHA512 (irqbalance-1.6.0.tar.gz) = ce89dc41eb3aab5950a56b4b1c817aae5e87eaa84bc69bf88618caf53386513e488bb0b4724a600ae175c96be509367cfc418c7f85531007f80e02f7de549b16
+SHA512 (irqbalance-1.7.0.tar.gz) = 2fa29c738275dda2fac38d78ca0ec4619826b72529e8eda7ad8ddcda5768bbf282e2260dddc16a8b46d4296289b25dc59684575ae1badca0c6c6ae30b1e9b5ea

im-0/fedora-rpm.irqbalance

commit 420a21b0404e4a6698cabe5f857d2ab1a958ca04

Restore environment file patch
Fixes: 76add4e1b859 ("Resolves: bz1866002")

irqbalance-1.6.0-env-file-path.patch
@@ -1,11 +1,0 @@
---- a/misc/irqbalance.service	2020-06-02 14:33:35.794592546 +0200
-+++ b/misc/irqbalance.service	2020-06-02 14:34:08.678922108 +0200
-@@ -5,7 +5,7 @@
- ConditionVirtualization=!container
- 
- [Service]
--EnvironmentFile=/path/to/irqbalance.env
-+EnvironmentFile=/etc/sysconfig/irqbalance
- ExecStart=/usr/sbin/irqbalance --foreground $IRQBALANCE_ARGS
- CapabilityBoundingSet=
- NoNewPrivileges=yes

irqbalance-1.7.0-env-file-path.patch
@@ -1,0 +1,25 @@
+From 4030b4204deac3f8ab0d560bff2d8ae692489558 Mon Sep 17 00:00:00 2001
+From: Ivan Mironov <mironov.ivan@gmail.com>
+Date: Sun, 1 Nov 2020 16:09:40 +0500
+Subject: [PATCH] Read environment from /etc/sysconfig/irqbalance
+
+---
+ misc/irqbalance.service | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+
+diff --git a/misc/irqbalance.service b/misc/irqbalance.service
+index e7a33361bd7a..5700d4c40002 100644
+--- a/misc/irqbalance.service
++++ b/misc/irqbalance.service
+@@ -6,7 +6,7 @@ ConditionVirtualization=!container
+ 
+ [Service]
+ EnvironmentFile=-/usr/lib/irqbalance/defaults.env
+-EnvironmentFile=-/path/to/irqbalance.env
++EnvironmentFile=/etc/sysconfig/irqbalance
+ ExecStart=/usr/sbin/irqbalance --foreground $IRQBALANCE_ARGS
+ CapabilityBoundingSet=
+ NoNewPrivileges=yes
+-- 
+2.28.0
+

irqbalance.spec
@@ -1,6 +1,6 @@
 Name:           irqbalance
 Version:        1.7.0
-Release:        5%{?dist}
+Release:        6%{?dist}
 Epoch:          2 
 Summary:        IRQ balancing daemon
 
@@ -23,6 +23,7 @@ Requires: numactl-libs
 
 ExcludeArch: s390 s390x
 
+Patch1: irqbalance-1.7.0-env-file-path.patch
 
 %description
 irqbalance is a daemon that evenly distributes IRQ load across
@@ -30,6 +31,7 @@ multiple CPUs for enhanced performance.
 
 %prep
 %setup -q
+%patch1 -p1
 
 %build
 ./autogen.sh
@@ -70,6 +72,9 @@ fi
 /sbin/chkconfig --del irqbalance >/dev/null 2>&1 || :
 
 %changelog
+* Tue Feb 09 2021 Ivan Mironov <mironov.ivan@gmail.com> - 2:1.7.0-6
+- Restore environment file patch
+
 * Tue Jan 26 2021 Fedora Release Engineering <releng@fedoraproject.org> - 2:1.7.0-5
 - Rebuilt for https://fedoraproject.org/wiki/Fedora_34_Mass_Rebuild
 